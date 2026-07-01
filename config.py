afb4b12e07481a6cbb9388a908cf855e1c749824
NWL <104101563+nwl1909@users.noreply.github.com>
2026-07-01 21:18:30 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+
+# GitHub
+
+OWNER = "muk-as"
+REPO = "DOTA2_WEB"
+
+TRACK_FOLDER = "dota2_web/steam_api"
+
+BRANCH = "master"
+
+# Telegram
+
+TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
+TELEGRAM_CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
+
+# State
+
+STATE_FILE = "state.json"
+
+# GitHub API
+
+GITHUB_API = "https://api.github.com"
